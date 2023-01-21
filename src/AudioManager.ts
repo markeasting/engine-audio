cb0b17149bd95ed5964834677adfcbbd5a8f7fb3
Mark <markeasting@gmail.com>
2023-01-21 16:48:37 +0100

    Added samples

--- src/AudioManager.ts
+++ src/AudioManager.ts
@@ -1,10 +1,14 @@
 
 const sources: Record<string, string> = {
     on_high: 'audio/BAC_Mono_onhigh.wav',
-    off_high: 'audio/BAC_Mono_offveryhigh.wav',
-
-    off_low: 'audio/BAC_Mono_offlow.wav',
     on_low: 'audio/BAC_Mono_onlow.wav',
+    off_high: 'audio/BAC_Mono_offveryhigh.wav',
+    off_low: 'audio/BAC_Mono_offlow.wav',
+
+    // on_high: 'audio/procar/on_midhigh {eed64b99-c102-43cf-834e-4e4cafa68fdc}.wav',
+    // on_low: 'audio/procar/on_low {0477930f-2954-45ee-8ac4-db4867fe1749}.wav',
+    // off_high: 'audio/procar/off_midhigh {092a60f7-c729-4d2c-979e-2e766ba42c6c}.wav',
+    // off_low: 'audio/procar/off_lower {05f28dcf-8251-4e6a-bc40-8099139ef81e}.wav',
 }
 
 export class DynamicAudioNode {
